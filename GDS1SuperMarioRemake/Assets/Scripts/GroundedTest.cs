7093956771744b4e978b4a884c917f98660e752c
Delcan <decbot100@gmail.com>
2022-03-04 20:31:46 +1100

    Movement controller - Jumping and falling

---
+++ GDS1SuperMarioRemake/Assets/Scripts/GroundedTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class GroundedTest : MonoBehaviour
+{
+    // Attached collider
+    Collider2D col;
+
+    // Size of the box to test in.
+    [SerializeField] Vector2 size;
+    // Distance from the ground to be considered "grounded" (keep in mind with sizes that aren't the same as the collider, this won't literally be the distance from the bottom of the collider to the ground)
+    [SerializeField] float distance;
+    // Layers to check for
+    [SerializeField] LayerMask layerMask;
+
+    private void Awake() {
+        col = GetComponent<Collider2D>();
+    }
+
+    public bool IsGrounded () {
+        RaycastHit2D hit = Physics2D.BoxCast(col.bounds.center, size, 0, Vector2.down, distance, layerMask);
+        return hit.collider != null;
+    }
+}
--- GDS1SuperMarioRemake/Assets/Scripts/Player/MarioMovementController.cs
+++ GDS1SuperMarioRemake/Assets/Scripts/Player/MarioMovementController.cs
@@ -2,44 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(GroundedTest))]
 public class MarioMovementController : MonoBehaviour
 {
     Rigidbody2D rb;
+
+    Vector2 velocity = new Vector2();
+    GroundedTest groundedTest;
 
     // Properties
     [SerializeField] float maxWalkSpeed; // 1 and 9/16 pixels/s in the original game
     [SerializeField] float maxRunSpeed;  // 2 and 9/16 p/s in the original game
     [SerializeField] float gravityStrength; // Would usually use Unity's inbuilt gravity but given the simplicity that doesn't seem worthwhile. 6/16ths of a pixel in smb
+    [SerializeField] float jumpImpulse; // 4 pixels in the original game
+    [SerializeField] float jumpImpulseRunning; // 5 pixels in the original game, though the actual behaviour is slightly tweaked for this project
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        groundedTest = GetComponent<GroundedTest>();
     }
 
-    private void FixedUpdate() {
+    // Using standard update to catch all input down/ups
+    private void Update() {
         Movement();
     }
 
     private void Movement() {
         // Take raw input axes
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Vector2 velocity = new Vector2();
-
+        
         // Set the speed based on if run is held
-        float speed = Input.GetButton("Run") ? maxRunSpeed : maxWalkSpeed;
+        float speed = (Input.GetButton("Run") ? maxRunSpeed : maxWalkSpeed);
         // Set X velocity
         velocity.x = speed * input.x;
         
+        // debug
+        if (Input.GetButtonDown("Jump")) Debug.Log(IsGrounded() ? "Grounded" : "Not grounded");
+        
         // Jumping/Falling
-        // TODO: IsGrounded() currently not properly implemented
         if (IsGrounded()) {
-            velocity.y = 0;
             if (Input.GetButtonDown("Jump")) {
                 Jump();
             }
         } else {
-            // New velocity should be the old velocity plus acceleration due to gravity
+            // Apply acceleration due to gravity
             // TODO: terminal velocity
-            velocity.y = rb.velocity.y - gravityStrength;
+            velocity.y -= gravityStrength * Time.deltaTime;  
         }
         
         // Apply velocity to rigidbody
@@ -47,11 +55,11 @@ public class MarioMovementController : MonoBehaviour
     }
 
     private void Jump () {
-        //TODO
+        velocity.y = jumpImpulse;
     }
 
     private bool IsGrounded () {
-        return false;
+        return groundedTest.IsGrounded();
     }
 
 
