6ce3493022770fc466ea345008d5ae3d266a7be3
CB120 <burgio120@gmail.com>
2022-03-07 01:06:08 +1100

    Goomba movement in, Collisions working, Pipe walls added
    Goombas will now move back and fourth between pipe collisions using onTriggerEnter.

---
+++ GDS1SuperMarioRemake/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float goombaSpeed;
+    private bool wallCollision = false;
+    public GameObject Mario;
+
+
+
+    
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (wallCollision == false)
+                {
+                    transform.Translate(Vector2.left * goombaSpeed * Time.deltaTime);
+                }
+                else if (wallCollision == true)
+                {
+                    transform.Translate(Vector2.right * goombaSpeed * Time.deltaTime);
+                }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pipe")
+        {
+            wallCollision ^= true; // Switch between true & false on collision (fuck me dead i just learnt about xor-equals)
+        }
+
+        if(collision.gameObject == Mario) // && Star != active)
+        {
+            // If mario is small, put death code
+            // If mario is big, downgrade him to small
+            // If mario has the flower. downgrade him to big
+           
+        }
+    }
+}
