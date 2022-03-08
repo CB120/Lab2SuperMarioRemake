5bcc8489cd9bd4cd58da4deaa3a8267a3e54f625
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-03-08 21:52:13 +1100

    End Cinematic Progress

--- GDS1SuperMarioRemake/Assets/Scripts/CinematicController.cs
+++ GDS1SuperMarioRemake/Assets/Scripts/CinematicController.cs
@@ -10,23 +10,60 @@ public class CinematicController : MonoBehaviour
 
     //Variables
     [HideInInspector]
-    public string marioState;
+    public string marioState = "small";
 
     bool atBottomOfPole = false;
 
     //References
+    public Sprite smallClimbing;
+    public Sprite largeClimbing;
+    public Sprite fireClimbing;
+    public Sprite largeFireClimbing;
+
     [HideInInspector]
     public GameObject endFlagReference;
 
+    SpriteRenderer sprite;
+    Animator animator;
+
     //Engine-called
     void Start(){
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
+        SetSprite();
     }
 
     void Update(){
+        UpdatePosition();
     }
 
 
     //Private methods
+        //Start
+    void SetSprite(){
+        switch (marioState){
+            case "large":
+                sprite.sprite = largeClimbing; break;
+            case "fire":
+                sprite.sprite = fireClimbing; break;
+            case "largeFire":
+                sprite.sprite = largeFireClimbing; break;
+            default:
+                sprite.sprite = smallClimbing; break;
+        }
+    }
 
+
+        //Update
+    void UpdatePosition(){
+        if (!atBottomOfPole){
+            if (transform.position.y > flagBase.y){
+                transform.Translate(0f, descentSpeed * Time.deltaTime * -1f, 0f);
+            } else {
+                atBottomOfPole = true;
+                //Play animation
+            }
+        }
+    }
 }
