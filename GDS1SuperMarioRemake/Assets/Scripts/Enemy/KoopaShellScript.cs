73b9b121d5494ea68afe46f9a5c91885d8f1da44
CB120 <burgio120@gmail.com>
2022-03-08 19:34:28 +1100

    MORE COLLISIONS TWEAKED, Koopa Shell fully functional (besides killing mario) and more minor tweaks

---
+++ GDS1SuperMarioRemake/Assets/Scripts/Enemy/KoopaShellScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaShellScript : MonoBehaviour
+{
+    public float ShellSpeed;
+    private bool wallCollision = false;
+    public bool InitiateMovement = false;
+    public GameObject Mario;
+
+
+    void Update()
+    {
+        if (InitiateMovement == true)
+        {
+            if (wallCollision == true)
+            {
+                transform.Translate(Vector2.left * ShellSpeed * Time.deltaTime);
+            }
+            else if (wallCollision == false)
+            {
+                transform.Translate(Vector2.right * ShellSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(6.5f);
+        Destroy(this.gameObject);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (InitiateMovement == false)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                InitiateMovement = true;
+                Debug.Log("Shellz");
+                StartCoroutine(SelfDestruct());
+            }
+        }
+
+            if (InitiateMovement == true)
+            {
+                if (collision.gameObject.tag == "Pipe")
+                {
+                    wallCollision ^= true;
+                }
+                if (collision.gameObject.tag == "Enemy")
+                {
+                    Destroy(collision.gameObject);
+                    Debug.Log("Hit with a shell");
+                }
+
+                if (collision.gameObject == Mario)
+                {
+                    // Kill mario 
+                }
+            }
+        }
+    }
+
+
