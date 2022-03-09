dacddc4500d18590cf0953ea189d2e9150fea9cb
CB120 <burgio120@gmail.com>
2022-03-09 18:26:30 +1100

    Mario death on garbage collection

--- GDS1SuperMarioRemake/Assets/Scripts/World/GarbageCollector.cs
+++ GDS1SuperMarioRemake/Assets/Scripts/World/GarbageCollector.cs
@@ -7,10 +7,16 @@ public class GarbageCollector : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<MarioStateController>().OnEnemyCollision();
+        }
         if (collision.gameObject.tag != "Player")
         {
             Destroy(collision.gameObject);
         }
+
+
     }
 
 }
